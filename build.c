7f1d9a1d461c9b5a9e0959353996de34847cbae3
Joshalosh <josh.jessopsmith@gmail.com>
2024-09-04 14:23:32 +1000

    Set up the dev environment

--- build.c
+++ build.c
@@ -48,7 +48,7 @@ typedef struct Context_Extra {
 // #include "oogabooga/examples/input_example.c"
 // #include "oogabooga/examples/sprite_animation.c"
 // #include "oogabooga/examples/window_test.c"
-#include "oogabooga/examples/offscreen_drawing.c"
+// #include "oogabooga/examples/offscreen_drawing.c"
 // #include "oogabooga/examples/threaded_drawing.c"
 
 // These examples require some extensions to be enabled. See top respective files for more info.
@@ -58,3 +58,4 @@ typedef struct Context_Extra {
 
 // This is where you swap in your own project!
 // #include "entry_yourepicgamename.c"
+#include "entry_abyxgame.c"
